b84c9523b1d2f503023e76028c58d1bf01c0dd74
Pavlo Sumkin <psumkin@mirantis.com>
2016-08-22 18:42:24 +0300

    Practice: pointer to interface

--- references/main.go
+++ references/main.go
@@ -12,19 +12,25 @@ import (
 type Identifiable interface {
 	GetID() uuid.UUID
 	SetID(uuid.UUID)
+	TouchID(uuid.UUID)
 }
 
-// LogID works on Identifiable
-func LogID(i Identifiable) {
-	log.Println("#LogID", i.GetID())
+// GetID works on Identifiable
+func GetID(i Identifiable) {
+	log.Println("#GetID", i.GetID())
 }
 
-// UpdateID works on Identifiable
-func UpdateID(i Identifiable) {
+// SetID works on Identifiable
+func SetID(i Identifiable) {
 	i.SetID(uuid.NewV4())
 }
 
-// Update works on *Identifiable - compiling error
+// TouchID works on Identifiable
+func TouchID(i Identifiable) {
+	i.TouchID(uuid.NewV4())
+}
+
+// Update works on *Identifiable
 func Update(i *Identifiable) {
 	(*i).SetID(uuid.NewV4())
 }
@@ -43,36 +49,49 @@ func NewRecord() *Record {
 	return p
 }
 
-// GetID implements Identifiable interface
+// GetID implements Identifiable interface on pointer
 func (r *Record) GetID() uuid.UUID {
 	return r.ID
 }
 
-// SetID implements Identifiable interface
+// SetID implements Identifiable interface on pointer
 func (r *Record) SetID(id uuid.UUID) {
 	r.ID = id
+}
+
+// TouchID implements Identifiable interface on struct
+func (r Record) TouchID(id uuid.UUID) {
+	r.ID = id
+	log.Println("#TouchID", r.ID)
 }
 
 // Records represents collection
 type Records []Record
 
+// Check types at compiling
+var _ Identifiable = &Record{}
+
 func main() {
 	rec1 := Record{uuid.NewV4()}
 	rec2 := NewRecord()
-	log.Println("#list", rec1, rec2)
+	log.Println("#list #1", rec1, rec2)
 
-	LogID(&rec1)
-	LogID(rec2)
+	GetID(&rec1)
+	GetID(rec2)
 
-	UpdateID(&rec1)
-	UpdateID(rec2)
-	LogID(&rec1)
-	LogID(rec2)
+	SetID(&rec1)
+	SetID(rec2)
+	log.Println("#list #2", rec1, rec2)
 
-	// Update(&rec1)
-	// Update(rec2)
-	// LogID(&rec1)
-	// LogID(rec2)
+	TouchID(&rec1)
+	TouchID(rec2)
+	log.Println("#list #3", rec1, rec2)
+
+	var i1, i2 Identifiable
+	i1 = &rec1
+	i2 = rec2
+	Update(&i1)
+	Update(&i2)
 
 	rr := Records{rec1, *rec2}
 	log.Println("#rr", rr)
