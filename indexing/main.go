f1e07d8b78282bd1a3d8a0217247b2ff09377371
Pavlo Sumkin <psumkin@mirantis.com>
2016-08-23 17:12:05 +0300

    Collections indexting

--- indexing/main.go
+++ indexing/main.go
@@ -4,51 +4,117 @@ package main
 
 import "log"
 
-// Lenable requeres implementation
-type Lenable interface {
+// Indexed requeres implementation
+type Indexed interface {
 	At(int) interface{}
 	Len() int
 }
 
-// LogLenableLen works on Lenable
-func LogLenableLen(l Lenable) {
-	log.Println("#LogLenableLen", l.Len())
+// LogIndexed works on Indexed
+func LogIndexed(l Indexed) {
+	// invalid argument l (type Indexed) for len
+	// log.Println("#IndexedLen", len(l))
+
+	log.Println("#IndexedLen", l.Len())
+
+	// invalid operation: l[0] (type Indexed does not support indexing)
+	// log.Println("#IndexedAt0", l[0])
+
+	log.Println("#IndexedAt0", l.At(0))
 }
 
-// LogLenable0 works on Lenable
-func LogLenable0(l Lenable) {
-	// invalid operation: l[0] (type Lenable does not support indexing)
-	// log.Println("#LogLenable0", l[0])
-
-	log.Println("#LogLenable0", l.At(0))
+// T represents example data
+type T struct {
+	i int
 }
 
-// Record represents example data
-type Record struct {
-	ID int
-}
+// Map represents collection
+type Map map[int]T
 
-// Records represents collection
-type Records []Record
-
-// At implements Lenable interface
-func (rr *Records) At(at int) interface{} {
-	if len(*rr) > at {
-		return (*rr)[at]
+// At implements Indexed interface
+func (tt Map) At(at int) interface{} {
+	if len(tt) > at {
+		return tt[at]
 	}
 	return nil
 }
 
-// Len implements Lenable interface
-func (rr *Records) Len() int {
-	return len(*rr)
+// Len implements Indexed interface
+func (tt Map) Len() int {
+	return len(tt)
+}
+
+// PMap represents collection
+type PMap map[int]*T
+
+// At implements Indexed interface
+func (tt PMap) At(at int) interface{} {
+	if len(tt) > at {
+		return tt[at]
+	}
+	return nil
+}
+
+// Len implements Indexed interface
+func (tt PMap) Len() int {
+	return len(tt)
+}
+
+// Slice represents collection
+type Slice []T
+
+// At implements Indexed interface
+func (tt Slice) At(at int) interface{} {
+	if len(tt) > at {
+		return tt[at]
+	}
+	return nil
+}
+
+// Len implements Indexed interface
+func (tt Slice) Len() int {
+	return len(tt)
+}
+
+// PSlice represents collection
+type PSlice []*T
+
+// At implements Indexed interface
+func (tt PSlice) At(at int) interface{} {
+	if len(tt) > at {
+		return tt[at]
+	}
+	return nil
+}
+
+// Len implements Indexed interface
+func (tt PSlice) Len() int {
+	return len(tt)
 }
 
 func main() {
-	rr := Records{{1}, {2}}
-	log.Println("#rr", rr, rr.Len())
-	log.Println("#rr[0]", rr[0])
+	map1 := Map{0: {11}, 1: {22}}
+	log.Println("#map1", map1, len(map1), map1[0])
 
-	LogLenableLen(&rr)
-	LogLenable0(&rr)
+	LogIndexed(map1)
+	LogIndexed(&map1)
+
+	pmap1 := PMap{0: {11}, 1: {22}}
+	log.Println("#pmap1", pmap1, len(pmap1), pmap1[0])
+
+	LogIndexed(pmap1)
+	LogIndexed(&pmap1)
+
+	slice1 := Slice{{11}, {22}}
+	log.Println("#slice1", slice1, len(slice1), slice1[0])
+
+	LogIndexed(slice1)
+	LogIndexed(&slice1)
+
+	pslice1 := PSlice{{11}, {22}}
+	log.Println("#pslice1", pslice1, len(pslice1), pslice1[0])
+
+	LogIndexed(pslice1)
+	LogIndexed(&pslice1)
+
 }
