1730f4738c406f07d3ccf5f58e5128f1706249ad
Pavlo Sumkin <psumkin@mirantis.com>
2016-08-23 18:41:23 +0300

    Methods on values or pointers

--- pointers/main.go
+++ pointers/main.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/satori/go.uuid"
 )
@@ -15,22 +15,18 @@ type Identifiable interface {
 	TouchID(uuid.UUID)
 }
 
-// GetID works on Identifiable
-func GetID(i Identifiable) {
-	log.Println("#GetID", i.GetID())
-}
-
-// SetID works on Identifiable
-func SetID(i Identifiable) {
-	i.SetID(uuid.NewV4())
-}
-
-// TouchID works on Identifiable
-func TouchID(i Identifiable) {
+// Identify works on Identifiable
+func Identify(i Identifiable) {
+	fmt.Println("#Identify", i.GetID())
 	i.TouchID(uuid.NewV4())
+	fmt.Println("#", i.GetID())
+	i.SetID(uuid.NewV4())
+	fmt.Println("#", i.GetID())
+	fmt.Println()
 }
 
 // Update works on *Identifiable
+// https://golang.org/doc/faq#pointer_to_interface
 func Update(i *Identifiable) {
 	(*i).SetID(uuid.NewV4())
 }
@@ -49,8 +45,8 @@ func NewRecord() *Record {
 	return p
 }
 
-// GetID implements Identifiable interface on pointer
-func (r *Record) GetID() uuid.UUID {
+// GetID implements Identifiable interface on struct
+func (r Record) GetID() uuid.UUID {
 	return r.ID
 }
 
@@ -60,9 +56,10 @@ func (r *Record) SetID(id uuid.UUID) {
 }
 
 // TouchID implements Identifiable interface on struct
+// https://golang.org/doc/faq#methods_on_values_or_pointers
 func (r Record) TouchID(id uuid.UUID) {
 	r.ID = id
-	log.Println("#TouchID", r.ID)
+	fmt.Println("#TouchID", r.ID)
 }
 
 // Records represents collection
@@ -74,18 +71,16 @@ var _ Identifiable = &Record{}
 func main() {
 	rec1 := Record{uuid.NewV4()}
 	rec2 := NewRecord()
-	log.Println("#list #1", rec1, rec2)
+	fmt.Println("#list #1", rec1, rec2)
 
-	GetID(&rec1)
-	GetID(rec2)
+	Identify(&rec1)
+	Identify(rec2)
 
-	SetID(&rec1)
-	SetID(rec2)
-	log.Println("#list #2", rec1, rec2)
+	fmt.Println("#list #2", rec1, rec2)
 
-	TouchID(&rec1)
-	TouchID(rec2)
-	log.Println("#list #3", rec1, rec2)
+	(*Record).SetID(&rec1, uuid.NewV4())
+	Identifiable.SetID(rec2, uuid.NewV4())
+	fmt.Println("#list #4", rec1, rec2)
 
 	var i1, i2 Identifiable
 	i1 = &rec1
@@ -94,5 +89,5 @@ func main() {
 	Update(&i2)
 
 	rr := Records{rec1, *rec2}
-	log.Println("#rr", rr)
+	fmt.Println("#rr", rr)
 }
